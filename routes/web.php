4f1cdb5ea076825cd0f396d8262af2e82e690e4b
Maxim Ezhov <m.yezhov@etpgpb.ru>
2021-08-19 14:31:52 +0500

    Working search string with Controller

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    Illuminate\Support\Facades\DB::listen(static function ($query) {
-        logger($query->sql, $query->bindings);
-    });
-    return view('posts', [
-        'posts' => Post::latest('id')->get(),
-        'categories' => Category::oldest('name')->get(),
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
