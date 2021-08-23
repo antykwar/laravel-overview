171084f262a224c19974de7504a70ed992cf604a
Maxim Ezhov <m.yezhov@etpgpb.ru>
2021-08-23 23:08:25 +0500

    Writing comments

---
+++ resources/views/components/submit-button.blade.php
@@ -0,0 +1,3 @@
+<button type="submit" class="bg-blue-400 text-white font-semibold rounded py-2 px-10 hover:bg-blue-500">
+    {{ $slot }}
+</button>
---
+++ resources/views/posts/_add_comment_form.blade.php
@@ -0,0 +1,37 @@
+@auth
+    <x-panel>
+        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+            @csrf
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" class="rounded-full" alt="" width="40"
+                     height="40"/>
+                <h2 class="ml-4">Want to participate?</h2>
+            </header>
+
+            <div class="mt-6">
+                <textarea
+                    name="body"
+                    class="w-full text-sm focus:outline-none focus:ring"
+                    rows="5"
+                    placeholder="Add your important comment here!"
+                    required
+                >
+                </textarea>
+            </div>
+
+            @error('body')
+                <span class="text-xs text-red-500">{{ $message }}</span>
+            @enderror
+
+            <div class="flex justify-end mt-6">
+                <x-submit-button>Post</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p>
+        <a href="/login" class="font-semibold hover:underline">Log in</a> or
+        <a href="/register" class="font-semibold hover:underline">Register</a>
+        to leave a comment.
+    </p>
+@endauth
