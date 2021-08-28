04f184d74bdff1e60e5c13df5640e000dc4e1f43
Maxim Ezhov <m.yezhov@etpgpb.ru>
2021-08-28 19:24:56 +0500

    Move Post Create form into separate component

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,10 +1,11 @@
 @props(['trigger'])
 
-<div x-data="{show:false}" @click.away="show = false;">
-  <div @click="show = !show;">
-    {{ $trigger }}
-  </div>
-  <div x-show="show" class="py-2 absolute bg-gray-100 w-full z-50 mt-2 rounded-xl overflow-auto max-h-52" style="display:none">
-    {{ $slot }}
-  </div>
+<div x-data="{show:false}" @click.away="show = false;" class="relative">
+    <div @click="show = !show;">
+        {{ $trigger }}
+    </div>
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full z-50 mt-2 rounded-xl overflow-auto max-h-52"
+         style="display:none">
+        {{ $slot }}
+    </div>
 </div>
